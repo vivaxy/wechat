3953db11cdc967443366ca15da083c7854c978f6
vivaxy <xyxuye2007@126.com>
2014-07-30 09:27:40 +0800

    测试微信分享

--- web/js/wechat.js
+++ web/js/wechat.js
@@ -18,42 +18,42 @@ function share(n) {
 function hiddenMe() {
     document.getElementById("sbg").className = "sbg";
 }
-//
-//var mebtnopenurl = 'http://vivaxy.github.io/wechat/web/';
-//document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-//
-//    WeixinJSBridge.on('menu:share:appmessage', function (argv) {
-//        WeixinJSBridge.invoke('sendAppMessage', {
-//            "img_url": window.shareData.imgUrl,
-//            "link": window.shareData.timeLineLink,
-//            "desc": window.shareData.tContent,
-//            "title": window.shareData.tTitle
-//        }, function (res) {
-//            document.location.href = mebtnopenurl;
-//        })
-//    });
-//
-//    WeixinJSBridge.on('menu:share:timeline', function (argv) {
-//        WeixinJSBridge.invoke('shareTimeline', {
-//            "img_url": window.shareData.imgUrl,
-//            "img_width": "640",
-//            "img_height": "640",
-//            "link": window.shareData.timeLineLink,
-//            "desc": window.shareData.tContent,
-//            "title": window.shareData.tTitle
-//        }, function (res) {
-//            document.location.href = mebtnopenurl;
-//        });
-//    });
-//
-//
-//    WeixinJSBridge.on('menu:share:weibo', function (argv) {
-//        WeixinJSBridge.invoke('shareWeibo', {
-//            "content": window.shareData.tContent,
-//            "url": window.shareData.timeLineLink
-//        }, function (res) {
-//            document.location.href = mebtnopenurl;
-//        });
-//    });
-//
-//}, false)
+
+var mebtnopenurl = 'http://vivaxy.github.io/wechat/web/';
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+
+    WeixinJSBridge.on('menu:share:appmessage', function (argv) {
+        WeixinJSBridge.invoke('sendAppMessage', {
+            "img_url": window.shareData.imgUrl,
+            "link": window.shareData.timeLineLink,
+            "desc": window.shareData.tContent,
+            "title": window.shareData.tTitle
+        }, function (res) {
+            document.location.href = mebtnopenurl;
+        })
+    });
+
+    WeixinJSBridge.on('menu:share:timeline', function (argv) {
+        WeixinJSBridge.invoke('shareTimeline', {
+            "img_url": window.shareData.imgUrl,
+            "img_width": "640",
+            "img_height": "640",
+            "link": window.shareData.timeLineLink,
+            "desc": window.shareData.tContent,
+            "title": window.shareData.tTitle
+        }, function (res) {
+            document.location.href = mebtnopenurl;
+        });
+    });
+
+
+    WeixinJSBridge.on('menu:share:weibo', function (argv) {
+        WeixinJSBridge.invoke('shareWeibo', {
+            "content": window.shareData.tContent,
+            "url": window.shareData.timeLineLink
+        }, function (res) {
+            document.location.href = mebtnopenurl;
+        });
+    });
+
+}, false)

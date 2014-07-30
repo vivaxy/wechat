9531724413da8d663c3afa1de168107b5b2be79e
vivaxy <xyxuye2007@126.com>
2014-07-30 09:25:03 +0800

    测试微信分享

--- web/js/wechat.js
+++ web/js/wechat.js
@@ -9,60 +9,51 @@ window.shareData = {
     "tTitle": "this is a title",
     "tContent": "this is a content"
 };
-
-function showme() {
-    if (navigator.userAgent.indexOf("iPhone") != -1) {
-        window.location.href = "http://mp.weixin.qq.com/s?__biz=MzA5NDY4MzMxOQ==&mid=200506249&idx=1&sn=600daa415b4d0c00e526e93bb9cd1bea#rd";
-    } else {
-        window.location.href = "http://wechat.nibaguai.com/cat/install.php";
-    }
-}
 function share(n) {
-    document.title = window.shareData.tTitle = "你输入了" + n;
+    document.title = window.shareData.tTitle = n;
     document.getElementById("sbg").className = "sbgshow";
     window.setTimeout(hiddenMe, 5000);
-    document.getElementById("myfav").style.display = "none";
 }
 
 function hiddenMe() {
     document.getElementById("sbg").className = "sbg";
 }
-
-var mebtnopenurl = 'http://vivaxy.github.io/wechat/web/';
-document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-
-    WeixinJSBridge.on('menu:share:appmessage', function (argv) {
-        WeixinJSBridge.invoke('sendAppMessage', {
-            "img_url": window.shareData.imgUrl,
-            "link": window.shareData.timeLineLink,
-            "desc": window.shareData.tContent,
-            "title": window.shareData.tTitle
-        }, function (res) {
-            document.location.href = mebtnopenurl;
-        })
-    });
-
-    WeixinJSBridge.on('menu:share:timeline', function (argv) {
-        WeixinJSBridge.invoke('shareTimeline', {
-            "img_url": window.shareData.imgUrl,
-            "img_width": "640",
-            "img_height": "640",
-            "link": window.shareData.timeLineLink,
-            "desc": window.shareData.tContent,
-            "title": window.shareData.tTitle
-        }, function (res) {
-            document.location.href = mebtnopenurl;
-        });
-    });
-
-
-    WeixinJSBridge.on('menu:share:weibo', function (argv) {
-        WeixinJSBridge.invoke('shareWeibo', {
-            "content": window.shareData.tContent,
-            "url": window.shareData.timeLineLink
-        }, function (res) {
-            document.location.href = mebtnopenurl;
-        });
-    });
-
-}, false)
+//
+//var mebtnopenurl = 'http://vivaxy.github.io/wechat/web/';
+//document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+//
+//    WeixinJSBridge.on('menu:share:appmessage', function (argv) {
+//        WeixinJSBridge.invoke('sendAppMessage', {
+//            "img_url": window.shareData.imgUrl,
+//            "link": window.shareData.timeLineLink,
+//            "desc": window.shareData.tContent,
+//            "title": window.shareData.tTitle
+//        }, function (res) {
+//            document.location.href = mebtnopenurl;
+//        })
+//    });
+//
+//    WeixinJSBridge.on('menu:share:timeline', function (argv) {
+//        WeixinJSBridge.invoke('shareTimeline', {
+//            "img_url": window.shareData.imgUrl,
+//            "img_width": "640",
+//            "img_height": "640",
+//            "link": window.shareData.timeLineLink,
+//            "desc": window.shareData.tContent,
+//            "title": window.shareData.tTitle
+//        }, function (res) {
+//            document.location.href = mebtnopenurl;
+//        });
+//    });
+//
+//
+//    WeixinJSBridge.on('menu:share:weibo', function (argv) {
+//        WeixinJSBridge.invoke('shareWeibo', {
+//            "content": window.shareData.tContent,
+//            "url": window.shareData.timeLineLink
+//        }, function (res) {
+//            document.location.href = mebtnopenurl;
+//        });
+//    });
+//
+//}, false)
